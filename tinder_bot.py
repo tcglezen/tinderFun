c8205e95465b0e18a1ac509852f12b3a79ffac05
Thomas Glezen <tcglezen@gmail.com>
2020-08-29 02:39:57 -0700

    updated tinder_bot so that it works with the newest tinder

--- tinder_bot.py
+++ tinder_bot.py
@@ -7,12 +7,58 @@ class TinderBot():
     def __init__(self):
         self.driver = webdriver.Chrome()
 
+
+    def newLogin(self):
+        self.driver.get('https://tinder.com')
+
+        sleep(2)
+        #Clicks the login button
+        fb_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+        fb_btn.click()
+
+        base_window = self.driver.window_handles[0]
+
+        #Offers to login using facebook
+        sleep(2)
+        #popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+        popup_1.click()
+
+        #Enters in private info so that we can login
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+        email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
+        email_in.send_keys(username)
+
+        pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        pw_in.send_keys(password)
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
+        login_btn.click()
+
+        #Goes back to original tinder window
+        self.driver.switch_to_window(base_window)
+
+        #gets rid of the some of the annoying garbage
+        sleep(4)
+        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_2.click()
+
+        sleep(0.5)
+        popup_3 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+        popup_3.click()
+
+        popup_ads = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+        popup_ads.click()
+
+
     def login(self):
         self.driver.get('https://tinder.com')
 
         sleep(2)
-
-        fb_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
+        #Clicks the login button
+        fb_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+        #fb_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
         fb_btn.click()
 
         # switch to login popup
@@ -29,15 +75,16 @@ class TinderBot():
         login_btn.click()
 
         self.driver.switch_to_window(base_window)
-
-        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+        #popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
         popup_1.click()
 
         popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
         popup_2.click()
 
     def like(self):
-        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
+        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        #like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
         like_btn.click()
 
     def dislike(self):
@@ -64,4 +111,4 @@ class TinderBot():
         match_popup.click()
 
 bot = TinderBot()
-bot.login()
+bot.newLogin()
